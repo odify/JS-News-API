03101cc4d994aca3d6145ba4350d65803bccc814
odify <odify495@gmail.com>
2020-05-26 22:26:48 +0200

    changed key

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,7 @@ function myFunx() {
   document.getElementById("content").innerHTML = "";
   console.log("content");
   fetch(
-    `https://newsapi.org/v2/everything?q=bitcoin&apiKey=f6a267260a564dc08d40a90e1a8be145`
+    `http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-26&sortBy=publishedAt&apiKey=f6a267260a564dc08d40a90e1a8be145`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -31,7 +31,7 @@ function myFuny() {
   document.getElementById("content").innerHTML = "";
   console.log("content");
   fetch(
-    `https://newsapi.org/v2/everything?q=corona&apiKey=f6a267260a564dc08d40a90e1a8be145`
+    `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f6a267260a564dc08d40a90e1a8be145`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -58,7 +58,7 @@ function myFunz() {
   document.getElementById("content").innerHTML = "";
   console.log("content");
   fetch(
-    `https://newsapi.org/v2/everything?q=sport&apiKey=f6a267260a564dc08d40a90e1a8be145`
+    `http://newsapi.org/v2/top-headlines?country=de&category=sports&apiKey=f6a267260a564dc08d40a90e1a8be145`
   )
     .then((response) => response.json())
     .then((data) => {
