e6f07ef52bdf1a6298bd0e833b4f6da587df9968
odify <odify495@gmail.com>
2020-05-26 22:36:56 +0200

    key changed xyz

--- assets/main.js
+++ assets/main.js
@@ -4,9 +4,7 @@
 function myFunx() {
   document.getElementById("content").innerHTML = "";
   console.log("content");
-  fetch(
-    `http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-26&sortBy=publishedAt&apiKey=f6a267260a564dc08d40a90e1a8be145`
-  )
+  fetch(`http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-26&sortBy=publishedAt&apiKey=f6a267260a564dc08d40a90e1a8be145`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data.articles);
@@ -28,18 +26,16 @@ function myFunx() {
 
 
 function myFuny() {
-  document.getElementById("content").innerHTML = "";
-  console.log("content");
-  fetch(
-    `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f6a267260a564dc08d40a90e1a8be145`
-  )
+  document.getElementById("contentx").innerHTML = "";
+  console.log("contentx");
+  fetch(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f6a267260a564dc08d40a90e1a8be145`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data.articles);
       let test = "unknown";
       for (let elt of data.articles) {
         console.log(elt);
-        document.getElementById("content").innerHTML += `
+        document.getElementById("contentx").innerHTML += `
                <article>
                <img src="${elt.urlToImage}">
                <h1>${elt.title}</h1>
@@ -55,18 +51,16 @@ function myFuny() {
 
 
 function myFunz() {
-  document.getElementById("content").innerHTML = "";
-  console.log("content");
-  fetch(
-    `http://newsapi.org/v2/top-headlines?country=de&category=sports&apiKey=f6a267260a564dc08d40a90e1a8be145`
-  )
+  document.getElementById("contenty").innerHTML = "";
+  console.log("contenty");
+  fetch(`http://newsapi.org/v2/top-headlines?country=de&category=sports&apiKey=f6a267260a564dc08d40a90e1a8be145`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data.articles);
       let test = "unknown";
       for (let elt of data.articles) {
         console.log(elt);
-        document.getElementById("content").innerHTML += `
+        document.getElementById("contenty").innerHTML += `
                <article>
                <img src="${elt.urlToImage}">
                <h1>${elt.title}</h1>
